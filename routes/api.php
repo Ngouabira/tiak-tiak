0ff85c4cd81a585c58a1c4dab0882addcac63350
Ngouabira <valdyngouabira@gmail.com>
2024-01-17 16:33:10 +0100

    Routes created.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\DeliveryRequestController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDeliveryController;
+use App\Http\Controllers\OrderDeliveryRequestController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +23,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+require __DIR__ . '/valdy.php';
+require __DIR__ . '/diop.php';
+require __DIR__ . '/seck.php';
+require __DIR__ . '/fatou.php';
+require __DIR__ . '/omar.php';
+require __DIR__ . '/dienaba.php';
+
+Route::apiResource('profile', ProfileController::class);
+Route::apiResource('delivery-request', DeliveryRequestController::class);
+Route::apiResource('delivery', DeliveryController::class);
+Route::apiResource('transaction', TransactionController::class);
+Route::apiResource('category', CategoryController::class);
+Route::apiResource('product', ProductController::class);
+Route::apiResource('order', OrderController::class);
+Route::apiResource('order-delivery-request', OrderDeliveryRequestController::class);
+Route::apiResource('order-delivery', OrderDeliveryController::class);
